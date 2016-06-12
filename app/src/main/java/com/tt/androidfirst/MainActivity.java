187ab96d38da6fe5edd393ff7383c4f63b39bc70
Liyao <liyaomt@126.com>
2016-06-12 17:24:41 +0800

    4

---
+++ app/src/main/java/com/tt/androidfirst/MainActivity.java
@@ -0,0 +1,16 @@
+package com.tt.androidfirst;
+
+import android.os.Bundle;
+
+/**
+ * Created by liyao on 2016/6/12.
+ */
+public class MainActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_layout);
+    }
+
+}
